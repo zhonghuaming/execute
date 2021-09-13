ad1fdf51f9853be530ff0fb58f10f7e02216e369
zhonghuaming <zhonghuaming@shencom.cn>
2021-09-14 07:37:04 +0800

    feat(flink): start

---
+++ src/main/java/cn/huaming/flink/count/StreamCount.java
@@ -0,0 +1,23 @@
+package cn.huaming.flink.count;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class StreamCount {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
+        DataStream<String> inputDataStream = env.socketTextStream(host, port);
+        DataStream<Tuple2<String, Integer>> wordCountDataStream = inputDataStream
+                .flatMap(new WordCount.MyFlatMapper())
+                .keyBy(0)
+                .sum(1);
+        wordCountDataStream.print().setParallelism(1);
+        env.execute();
+    }
+}
